dddc2e0d097b94535af8d7ad30d903d876abaaff
Killian-Habasque <88286789+Killian-Habasque@users.noreply.github.com>
2022-01-03 15:30:27 +0100

    v4

---
+++ modules/views/header.php
@@ -0,0 +1,29 @@
+<header>
+    <div class="logo">
+        <img src="" alt="">
+    </div>
+
+    <div class="grid">
+
+        <div>
+            <button class="son">
+                <img src="" alt="">
+            </button>
+
+        </div>
+
+        <div>
+            <button class="language">
+                <img src="" alt="">
+            </button>
+
+        </div>
+
+        <div>
+            <button class="burger">
+                <img src="" alt="">
+            </button>
+
+        </div>
+    </div>
+</header>
