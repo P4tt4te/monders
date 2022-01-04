202eb5f7400c63c3fe95311bbfed3b97176780e1
Killian-Habasque <88286789+Killian-Habasque@users.noreply.github.com>
2022-01-04 12:56:08 +0100

    v7

--- modules/views/header.php
+++ modules/views/header.php
@@ -1,24 +1,22 @@
+<!-- HEADER -->
 <header>
-
     <div class="logo">
-        <img src="../images/Logo.svg" alt="">
+        <a href="home.php">
+            <img src="../images/Logo.svg" alt="">
+        </a>
     </div>
-
     <div class="grid">
-
         <div class="grid-item">
             <button class="son">
-                <img src="../images/son.svg" alt="">
+                <img src="../images/sonOff.svg" alt="">
             </button>
         </div>
-
         <div class="grid-item">
             <button class="language">
                 <span>FR</span>
             </button>
 
         </div>
-
         <div class="grid-item">
             <button class="burger">
                 <span></span>
@@ -26,5 +24,33 @@
             </button>
         </div>
     </div>
-
 </header>
+
+<!-- MENU RESPONSIVE -->
+<div class="nav-container">
+    <div class="menu-close">CLOSE</div>
+
+    <div class="grid">
+        <div class="grid-item">
+            <nav class="menu">
+                <div class="menu-item">
+                    <a href="index.html" class="menu-item-link">PORTFOLIO</a>
+                </div>
+                <div class="bar-menu"></div>
+                <div class="menu-item">
+                    <a href="projects.html" class="menu-item-link">PROJECTS</a>
+                </div>
+                <div class="menu-item">
+                    <a href="about.html" class="menu-item-link">ABOUT</a>
+                </div>
+                <div class="menu-item">
+                    <a href="contact.html" class="menu-item-link">CONTACT</a>
+                </div>
+            </nav>
+        </div>
+        <div class="grid-item"></div>
+        <div class="grid-item"></div>
+    </div>
+</div>
+
+</div>
