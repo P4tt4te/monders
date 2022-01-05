4900bee5c97f428a731d9d17c2a5b1e9b1de9d60
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2022-01-05 17:08:54 +0100

    json quiz

--- modules/script/quiz-question.js
+++ modules/script/quiz-question.js
@@ -1,8 +1,25 @@
 'use strict';
-document.addEventListener("DOMContentLoaded",initialiser);
-function initialiser(e)
-{
+window.addEventListener("load",init);
 
+function init() {
+    recupjson();
+    console.log('test');
+}
 
+function recupjson() {
+    var config = { method: 'GET',
+               mode: 'cors',
+               cache: 'default' };
 
-};
+    fetch('../../public/quiz/tajMahal.json', config)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(tab) {
+        console.log(tab.question);
+    })
+    .catch(function(error) {
+        console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+      })
+    ;
+}
