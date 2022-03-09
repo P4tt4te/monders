72db0565d320061e0c7898cf1592992305249de1
Edward Brunetiere <edward.brunetiere@gmail.com>
2022-03-09 13:00:04 +0100

    nombre romains

--- modules/script/calcul.js
+++ modules/script/calcul.js
@@ -1,9 +1,34 @@
 window.addEventListener('load', init);
 var nbr = [6,7,8,9,10];
-var nbrromains = ["VI","VII","VII","IX","X"];
+var nbrromains = ["VI","VII","VIII","IX","X"];
+var source = document.querySelector('.romain');
+var zone = document.querySelector('.inputromain');
+var index = 0;
+var score = 0;
 
 function init() {
-    for(let i = 0;i<5; i++) {
+    
+    zone.addEventListener('change',check);
+    source.textContent = nbrromains[0];
+    
+}
 
+function check(evt) {
+    console.log(evt.target.value);
+    if (evt.target.value == nbr[index]) {
+        console.log('oui');
+        score++;
+        evt.target.value = null;
+    } else {
+        console.log('non');
+        evt.target.value = null;
+    }
+    index++;
+    if (index > 4) {
+        zone.removeEventListener('change',check);
+        console.log('Score : '+score);
+    } else {
+        source.textContent = nbrromains[index];
+        document.querySelector('.indexquestion').textContent = index + 1;
     }
 }
