6a8be6bc0fbacaad6efcb2d335e148995199700f
Edward Brunetiere <edward.brunetiere@gmail.com>
2022-03-10 10:29:46 +0100

    a

--- modules/views/quiz.php
+++ modules/views/quiz.php
@@ -1,41 +1,21 @@
-<<<<<<< HEAD:modules/views/quiz.php
-<<<<<<< Updated upstream:modules/views/quiz.html
-<link rel="stylesheet" href="/modules/styles/quizz.css">
-<link rel="stylesheet" href="/style.css">
-=======
-<?php
-
-require_once('../controllers/quiz.php');
-require_once('../views/pageBegin.php');
-require_once('../views/header.php');
-
-?>
-
-
-
-
-<link rel="stylesheet" href="../styles/quizz.css">
-<link rel="stylesheet" href="../styles/style.css">
->>>>>>> Stashed changes:modules/views/quiz.php
-=======
 
 <?php 
 require_once('../views/common.php');
 require_once('../views/pageBegin.php');
 require_once('../views/header.php');
 
-
+if (!isset($_SESSION['user']) )
+	error("Veuillez vous connecter pour accéder à la ressource",  "../views/inscription.php");
 ?>
 
 <link rel="stylesheet" href="../styles/quizz.css">
-
->>>>>>> nathan:modules/views/quiz.html
+<img src="../images/quiz/images/cookie.png">
 <main>
     <div class="container">
         <div class="intitule">
             <h1 class="title">QUIZ</h1>
-            <div class="descriptif">
-                <h2 class="nommerveille light">Taj Mahal <?php echo("succes") ?></h2>
+            <div class="descriptif bold">
+                <h2 class="nommerveille light">Taj Mahal</h2>
                 <p>10 questions</p>
                 <p class="difficulte"> niveau difficile </p>
             </div>
@@ -46,17 +26,9 @@ require_once('../views/header.php');
         </form>
     </div>
     <div class="divretour">
-<<<<<<< HEAD:modules/views/quiz.php
-<<<<<<< Updated upstream:modules/views/quiz.html
-        <a href="page précédente">
-=======
-        <a href="../views/tajMahal.html">
->>>>>>> Stashed changes:modules/views/quiz.php
-=======
 
         <a href="page précédente">
-        <a href="../views/tajMahal.html">
->>>>>>> nathan:modules/views/quiz.html
+        <a href="../views/tajMahal.php">
             <button class="retour" type="button"></button>
         </a>
     </div>
